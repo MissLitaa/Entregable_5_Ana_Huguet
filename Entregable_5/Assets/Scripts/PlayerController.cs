52ff1a2aa81d8e49066e2c832334d68580ef543f
MissLitaa <anahug49@gmail.com>
2021-11-15 11:21:26 +0100

    entregable5
    entregable5

---
+++ Entregable_5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    public float turnSpeed = 20.0f;
+    public float verticalInput;
+    private float zMax = 450.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Posición 0,0,0.
+        transform.position = Vector3.zero;
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * verticalInput);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * -verticalInput);
+        }
+
+        if (transform.position.z >= zMax)
+        {
+            Debug.Log("THE END");
+            Time.timeScale = 0;
+        }
+    }
+}
